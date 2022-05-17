ac3aecb62d64d9aeb69f7c268a15b55e1fba8084
reganan <reganan@users.noreply.github.com>
2022-05-17 17:13:57 +0800

    Add files via upload

---
+++ js/index2.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index.js";function e(){return r.exports.jsx("div",{children:r.exports.jsx("h2",{children:"Dashboard page"})})}export{e as default};
---
+++ js/index4.js
@@ -0,0 +1,1 @@
+import{j as s,H as e}from"./index.js";const r=({name:r})=>s.exports.jsxs("div",{children:[s.exports.jsx("h2",{children:"404"}),s.exports.jsx("div",{children:s.exports.jsx(e,{to:"/",children:"Home"})}),s.exports.jsx("div",{children:s.exports.jsx(e,{to:"/dashboard",children:"Dashboard"})})]});export{r as default};
